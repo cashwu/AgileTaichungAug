7819c5835345cc9a75b9ac8ae5dfce756fa59b0f
Cash Wu <cash@cashwu.com>
2016-08-21 21:28:14 +0800

    add CalculatorJS Test

---
+++ CalculatorJS/features/calculatorStep.js
@@ -0,0 +1,25 @@
+
+var Calculator = require('../app/calculator');
+var calculator = new Calculator();
+
+var chai = require('chai');
+var should = chai.should();
+
+module.exports = function() {
+
+    this.Given(/^: 輸入第一個數字 "([^"]*)"$/, function(first) {
+        this.first = +first;
+    });
+
+    this.Given(/^: 輸入第二個數字 "([^"]*)"$/, function(second) {
+        this.second = +second;
+    });
+
+    this.Given(/^: 計算時$/, function() {
+        this.acutal = calculator.Add(this.first, this.second);
+    });
+
+    this.Given(/^: 結果是 "([^"]*)"$/, function(expacted) {
+        (+expacted).should.equal(this.acutal);
+    });
+}
