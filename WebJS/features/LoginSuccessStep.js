797b08baa392ecc123ba3a89742ff29461958158
Cash Wu <cash@cashwu.com>
2016-08-22 02:13:13 +0800

    add WebJS test

---
+++ WebJS/features/LoginSuccessStep.js
@@ -0,0 +1,22 @@
+module.exports = function() {
+
+    this.Given(/^: 輸入使用者名稱 "([^"]*)"$/, function(userName) {
+        this.url("http://localhost:14058/Account/Login")
+            .waitForElementVisible('body', 1000);
+
+        this.setValue("input[id=UserName]", userName);
+    });
+
+    this.Given(/^: 輸入密碼 "([^"]*)"$/, function(password) {
+       this.setValue("input[id=Password]", password);
+    });
+
+    this.Given(/^: 登入時$/, function() {
+        this.click("input[type=submit]");
+    });
+
+    this.Given(/^: 成功導到 Index 頁面$/, function() {
+        var expect = "http://localhost:14058/Account";
+        this.assert.urlEquals(expect);
+    });
+};
