f048da2846d43ef92549f8be38904e087db6079a
Cash Wu <cash@cashwu.com>
2016-08-27 10:01:39 +0800

    remove specflow runner package

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -5,25 +5,23 @@ namespace Web.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Login
-        public ActionResult Login()
+        public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
             if (model.UserName == "Cash" && model.Password == "1234")
             {
-                return RedirectToAction("Index", "Account");
+                return RedirectToAction("Index");
             }
 
-            return View();
-        }
-
-        public ActionResult Index()
-        {
             return View();
         }
     }
--- Web/Models/LoginViewModel.cs
+++ Web/Models/LoginViewModel.cs
@@ -2,10 +2,10 @@
 {
     public class LoginViewModel
     {
+        public int Id { get; set; }
+
         public string UserName { get; set; }
 
         public string Password { get; set; }
-
-        public int Id { get; set; }
     }
 }
